50c5fe5ce9dbc266b33b03aa4fc198443391a02f
Sarkis M <melkonsa@gmail.com>
2016-07-12 18:26:42 -0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+  var windowH = $(window).height(),
+      introEl = $('div.intro'),
+      introHeadingH = introEl.find('h2').height();
+
+
+  introEl.css('padding', (windowH - introHeadingH)/2 + 'px 0');
+
+  $(document).on('scroll', function() {
+    introEl.slideUp(1000, function() {
+      $(document).off('scroll');
+    });
+  });
+});
